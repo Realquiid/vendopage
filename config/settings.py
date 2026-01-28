64fb727b593ae261edf48ec59c5758a72cb7e338
Realquiid <richardikenna61@gmail.com>
2026-01-28 06:53:10 +0100

    Switch to port 587 for Railway compatibility

--- config/settings.py
+++ config/settings.py
@@ -172,8 +172,12 @@ else:
     # For production - send emails via SMTP
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = config('EMAIL_HOST', default='smtp.hostinger.com')
-    EMAIL_PORT = config('EMAIL_PORT', default=465, cast=int)
-    EMAIL_USE_SSL = True   
+    # EMAIL_PORT = config('EMAIL_PORT', default=465, cast=int)
+    # EMAIL_USE_SSL = True   
+    # EMAIL_TIMEOUT = 10
+    EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = False
     EMAIL_TIMEOUT = 10
     EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
