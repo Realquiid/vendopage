e757052759e662cf53b5aa7334647605f32051af
Realquiid <richardikenna61@gmail.com>
2026-06-21 23:58:17 +0100

    prepare for digitalocean deploy

--- sellers/models.py
+++ sellers/models.py
@@ -7,10 +7,17 @@ from django.utils import timezone
 from cloudinary.models import CloudinaryField
 import uuid
 
-
 class SellerManager(UserManager):
     def create_user(self, email=None, password=None, **extra_fields):
-        return super().create_user(username=None, email=email, password=password, **extra_fields)
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
     def create_superuser(self, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
@@ -18,6 +25,7 @@ class SellerManager(UserManager):
         return self.create_user(email=email, password=password, **extra_fields)
 
 
+        
 class Seller(AbstractUser):
     SUBSCRIPTION_CHOICES = [
         ('free', 'Free'),
